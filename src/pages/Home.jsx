4e59ee94d1fce3c84df27d50ede93e18a1b78959
Nandiehakuna <gakiikelly403@gmail.com>
2024-05-22 10:30:01 +0300

    vercel environment variable added succefully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books`)
+      .get(`${process.env.REACT_APP_BACKEND_URL}/books`)
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
